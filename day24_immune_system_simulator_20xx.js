4d8ac34e7e6c79b98c257f509dac061185525d99
Adam Romig <adam.romig@gmail.com>
2019-01-18 18:05:09 -0500

    Day 24 part 2 finished

--- day24_immune_system_simulator_20xx.js
+++ day24_immune_system_simulator_20xx.js
@@ -4,15 +4,6 @@
 const file = require('./file');
 const input = file.read('day24').split(/\n/);
 
-/* const input = `Immune System:
-17 units each with 5390 hit points (weak to radiation, bludgeoning) with an attack that does 4507 fire damage at initiative 2
-989 units each with 1274 hit points (immune to fire; weak to bludgeoning, slashing) with an attack that does 25 slashing damage at initiative 3
-
-Infection:
-801 units each with 4706 hit points (weak to radiation) with an attack that does 116 bludgeoning damage at initiative 1
-4485 units each with 2961 hit points (immune to radiation; weak to fire, cold) with an attack that does 12 slashing damage at initiative 4`.split(/\n/); */
-
-
 class group {
     constructor (units, hp, atk, type, init, weakness, immunity) {
         this.units = units;                 // int
@@ -29,78 +20,123 @@ class group {
 
 const TEAM_IMMUNE_SYS = 0;
 const TEAM_INFECTION = 1;
+
+// Part 1
+
 let IMMUNE_SYS = [];
 let INFECTION = [];
-
 parseInput(input);
 
-let ImmuneLeft = IMMUNE_SYS.length;
-let InfectionLeft = INFECTION.length;
+let [teamImmuneLeft, teamInfectionLeft] = runSimulation();
 
-while (ImmuneLeft > 0 && InfectionLeft > 0) {
-    // target phase
-    const sortByPower = (grp) => {
-        return grp.concat().sort((a, b) => {
-            if (calcPower(b) === calcPower(a)) {
-                return b.initiative - a.initiative;
-            }
-            return calcPower(a) < calcPower(b) ? 1 : -1;
-        });
-    }
-    
-    for (grp of sortByPower(IMMUNE_SYS)) {
-        if (grp.units > 0)
-            chooseTarget(TEAM_IMMUNE_SYS, grp, INFECTION);
-    }
-    
-    for (grp of sortByPower(INFECTION)) {
-        if (grp.units > 0)
-            chooseTarget(TEAM_INFECTION, grp, IMMUNE_SYS);
-    }
-
-    // attack phase
-    const initiatives = [
-        ...IMMUNE_SYS.filter(x => x.units > 0)
-        .map(x => x.initiative),
-        ...INFECTION.filter(x => x.units > 0)
-        .map(x => x.initiative)
-    ].sort((a, b) => b - a);
-
-    for (const init of initiatives) {
-        let attacker = IMMUNE_SYS.find(x => x.initiative === init);
-        let team = (attacker !== undefined) ? TEAM_IMMUNE_SYS : TEAM_INFECTION;
-        if (team === TEAM_INFECTION)
-            attacker = INFECTION.find(x => x.initiative === init);
-
-        if (attacker.targetIndex === null)
-            continue;
-
-        const defender = (team === TEAM_IMMUNE_SYS) ?
-                            INFECTION[attacker.targetIndex] :
-                            IMMUNE_SYS[attacker.targetIndex];
-
-        const damage = calcDamage(attacker, defender);
-        
-        const deaths = Math.min(Math.floor(damage / defender.hp), defender.units);
-        defender.units -= deaths;
-        
-        let attackerIndex = (team === TEAM_IMMUNE_SYS) ?
-            IMMUNE_SYS.findIndex(x => x === attacker) :
-            INFECTION.findIndex(x => x === attacker);
-
-        attacker.targetIndex = null;
-        defender.targeted = false;
-    }
-
-    ImmuneLeft = IMMUNE_SYS.filter(x => x.units > 0).reduce((acc, x) => acc + x.units, 0);
-    InfectionLeft = INFECTION.filter(x => x.units > 0).reduce((acc, x) => acc + x.units, 0);
-}
-
-const winningArmy = ImmuneLeft > 0 ?
-                    IMMUNE_SYS.reduce((acc, x) => acc + x.units, 0) :
-                    INFECTION.reduce((acc, x) => acc + x.units, 0);
+const winningArmy = teamImmuneLeft > 0 ? teamImmuneLeft : teamInfectionLeft;
 
 console.log(`Part 1: ${winningArmy}`);
+
+// Part 2
+
+let boost = 1;
+
+let infectionLeft = 1;
+let immuneLeft;
+
+while (true)  {
+    IMMUNE_SYS = [];
+    INFECTION = [];
+    parseInput(input);
+    
+    infectionLeft = INFECTION.reduce((acc, x) => acc + x.units, 0);
+    
+    for (grp of IMMUNE_SYS) {
+        grp.atk += boost;
+    }
+    
+    // console.log(`Boosting atk by ${boost}...`);
+    [immuneLeft, infectionLeft] = runSimulation();
+    
+    if (infectionLeft === 0) {
+        // console.log(immuneLeft, infectionLeft);
+        break;
+    }
+
+    boost++;
+}
+
+console.log(`Part 2: ${immuneLeft}`);
+
+// Functions
+
+function runSimulation() {
+    let ImmuneLeft = IMMUNE_SYS.length;
+    let InfectionLeft = INFECTION.length;
+
+    while (ImmuneLeft > 0 && InfectionLeft > 0) {
+        // target phase
+        const sortByPower = (grp) => {
+            return grp.concat().sort((a, b) => {
+                if (calculatePower(b) === calculatePower(a)) {
+                    return b.initiative - a.initiative;
+                }
+                return calculatePower(a) < calculatePower(b) ? 1 : -1;
+            });
+        }
+        
+        for (grp of sortByPower(IMMUNE_SYS)) {
+            if (grp.units > 0)
+                chooseTarget(TEAM_IMMUNE_SYS, grp, INFECTION);
+        }
+        
+        for (grp of sortByPower(INFECTION)) {
+            if (grp.units > 0)
+                chooseTarget(TEAM_INFECTION, grp, IMMUNE_SYS);
+        }
+
+        // attack phase
+        const initiatives = [
+            ...IMMUNE_SYS.filter(x => x.units > 0)
+            .map(x => x.initiative),
+            ...INFECTION.filter(x => x.units > 0)
+            .map(x => x.initiative)
+        ].sort((a, b) => b - a);
+
+        let totalKills = 0;
+
+        for (const init of initiatives) {
+            let attacker = IMMUNE_SYS.find(x => x.initiative === init);
+            let team = (attacker !== undefined) ? TEAM_IMMUNE_SYS : TEAM_INFECTION;
+            
+            if (team === TEAM_INFECTION)
+                attacker = INFECTION.find(x => x.initiative === init);
+
+            if (attacker.targetIndex === null)
+                continue;
+
+            const defender = (team === TEAM_IMMUNE_SYS) ?
+                                INFECTION[attacker.targetIndex] :
+                                IMMUNE_SYS[attacker.targetIndex];
+
+            const damage = calculateDamage(attacker, defender);
+            
+            const deaths = Math.min(Math.floor(damage / defender.hp), defender.units);
+            defender.units -= deaths;
+            totalKills += deaths;
+
+            attacker.targetIndex = null;
+            defender.targeted = false;
+        }
+
+        ImmuneLeft = IMMUNE_SYS.filter(x => x.units > 0).reduce((acc, x) => acc + x.units, 0);
+        InfectionLeft = INFECTION.filter(x => x.units > 0).reduce((acc, x) => acc + x.units, 0);
+
+        // If no group has suffered any deaths, break out to avoid infinite loop.
+        if (totalKills === 0) {
+            // console.log('Stalemate, skipping...');
+            return [ImmuneLeft, InfectionLeft];
+        }
+    }
+
+    return [ImmuneLeft, InfectionLeft];
+}
 
 function parseInput(input) {
     const regexInfection = /Infection:/;
@@ -119,12 +155,8 @@ function parseInput(input) {
             [units, hp, atk, initiative] = [units, hp, atk, initiative].map(x => +x);
             let weaknesses = ((resists || '').match(regexWeak) || []).slice(1);
             let immunities = ((resists || '').match(regexImmune) || []).slice(1);
-            if (weaknesses.length > 0) {
-                weaknesses = [...weaknesses[0].split(', ')];
-            }
-            if (immunities.length > 0) {
-                immunities = [...immunities[0].split(', ')];
-            }
+            if (weaknesses.length > 0) { weaknesses = [...weaknesses[0].split(', ')]; }
+            if (immunities.length > 0) { immunities = [...immunities[0].split(', ')]; }
             
             if (team === TEAM_IMMUNE_SYS) {
                 IMMUNE_SYS.push(new group(
@@ -145,13 +177,13 @@ function chooseTarget(team, attacker, defenders) {
 
     for (const defender of defenders) {
         if (defender.units !== 0 && defender.targeted === false) {
-            const damage = calcDamage(attacker, defender);
-            if (damage > maxDamage) {
+            const damage = calculateDamage(attacker, defender);
+            if (damage > maxDamage && damage > 0) {
                 maxDamage = damage;
                 currentTarget = defender;
             } else if (damage === maxDamage) {
-                const defenderPower = calcPower(defender);
-                const currentTargetPower = calcPower(currentTarget);
+                const defenderPower = calculatePower(defender);
+                const currentTargetPower = calculatePower(currentTarget);
                 if (defenderPower > currentTargetPower) {
                     currentTarget = defender;
                 } else if (defenderPower === currentTargetPower) {
@@ -174,16 +206,16 @@ function chooseTarget(team, attacker, defenders) {
     }
 }
 
-function calcPower(grp) {
+function calculatePower(grp) {
     return grp.units * grp.atk;
 }
 
-function calcDamage(attacker, defender) {
+function calculateDamage(attacker, defender) {
     if (defender.immunities.includes(attacker.type)) {
         return 0;
     } else if (defender.weaknesses.includes(attacker.type)) {
-        return 2 * calcPower(attacker);
+        return 2 * calculatePower(attacker);
     } else {
-        return calcPower(attacker);
+        return calculatePower(attacker);
     }
 }
